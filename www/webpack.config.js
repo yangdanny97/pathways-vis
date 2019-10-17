8e759eeeca9b73c5f69dfe1028f11d6eeeb60f78
Sam Fuchs <samcfuchs@gmail.com>
2019-10-17 00:19:02 -0400

    Use sass stylesheets with bootstrap

--- www/webpack.config.js
+++ www/webpack.config.js
@@ -1,8 +1,29 @@
+const path = require('path');
+
 module.exports = {
-    mode: 'development',
-    entry: './src/splash.js',
-    output: {
-      filename: 'splash.js',
-      publicPath: 'dist'
-    }
+  mode: 'development',
+  entry: './src/splash.js',
+  output: {
+    filename: 'splash.js',
+    publicPath: 'dist'
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+  
+        use: [
+          { loader: "style-loader", },
+          { loader: "css-loader", },
+          { 
+            loader: "sass-loader",
+            options: {
+              implementation: require("sass")
+            }
+          }
+        ]
+      }
+    ],
+  },
 };
