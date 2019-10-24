36ee80236b1dabe9282025fe513e9a86bbf1a37b
yangdanny97 <dzy4@cornell.edu>
2019-10-23 23:43:09 -0400

    integrated splash page

--- www/src/splash.js
+++ www/src/splash.js
@@ -1,13 +1,13 @@
 var $ = require('jquery')
 
-import './splash.scss';
+import '../style/splash.scss';
 
 var input = $(".form-control")[0];
 let major = input.value;
 
 function go() {
     major = $('input')[0].value
-    window.location = 'pathways.html?major=' + major;
+    window.location = 'vis/?major=' + major;
 }
 
 window.go = go;
