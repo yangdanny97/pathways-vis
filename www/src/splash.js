154c0823f68ca9f2c7a17a6f05a9dd80b246ded2
Sam Fuchs <samcfuchs@gmail.com>
2019-11-08 11:40:50 -0500

    Add some typeahead interaction

--- www/src/splash.js
+++ www/src/splash.js
@@ -1,13 +1,21 @@
 var d3 = require("d3-selection");
+var Typeahead = require("typeahead")
 
 import '../style/splash.scss';
+import '../node_modules/typeahead/style.css';
 
-var input = d3.select(".form-control").node();
-let major = input.value;
+var major_input = d3.select("#major").node();
 
-function go() {
-    major = input.value
-    window.location = 'vis/?major=' + major;
-}
+var req = new Request("/majors/", { method: 'POST', });
+var majors = fetch(req)
+    .then(resp => resp.json())
+    .then(function(json) {
+        var names = json.map(obj => obj.Major);
+        console.log(names);
 
-window.go = go;
+        var ta = Typeahead(major_input, { source: names });
+        return majors
+    });
+
+
+
