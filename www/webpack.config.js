29b8429bebc5ab5986feb3b9fb16f11c0d26a90a
Sam Fuchs <samcfuchs@gmail.com>
2019-12-04 17:46:18 -0500

    Load CSS statically

--- www/webpack.config.js
+++ www/webpack.config.js
@@ -1,13 +1,25 @@
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-  mode: 'development',
+  mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
+
+  plugins: [
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // all options are optional
+      filename: '../css/[name].css',
+      chunkFilename: '[id].css',
+      ignoreOrder: false, // Enable to remove warnings about conflicting order
+    }),
+  ],
 
   entry: {
     splash: './src/splash.js',
     pathways: './src/pathways.js',
-    about: './src/about.js',
+    about: './src/about.js'
   },
+
   output: {
     filename: '[name].bundle.js',
     path: __dirname + '/static/dist'
@@ -19,14 +31,12 @@ module.exports = {
         test: /\.s?css$/,
   
         use: [
-          { loader: "style-loader", },
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: { publicPath: "static" },
+          },
           { loader: "css-loader", options: {url: false} },
-          { 
-            loader: "sass-loader",
-            options: {
-              implementation: require("sass")
-            }
-          }
+          "sass-loader",
         ]
       }
     ],
@@ -36,5 +46,6 @@ module.exports = {
     fs: 'empty',
     net: 'empty',
     tls: 'empty'
-  }
+  },
+
 };
