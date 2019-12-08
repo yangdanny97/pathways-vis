f54be5420a86617c2aea283680693677c25658a7
yangdanny97 <dzy4@cornell.edu>
2019-12-08 15:26:14 -0500

    better logging setup

--- www/logging/logging_setup.go
+++ www/logging/logging_setup.go
@@ -2,12 +2,28 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
 
 	_ "github.com/mattn/go-sqlite3"
 )
 
 func main() {
-	database, _ := sql.Open("sqlite3", "./pathways_logging.db")
-	statement, _ := database.Prepare("CREATE TABLE IF NOT EXISTS logs (id INTEGER PRIMARY KEY, user TEXT, log TEXT)")
-	statement.Exec()
+	database, err := sql.Open("sqlite3", "./pathways_logging.db")
+	if err != nil {
+		fmt.Println(err)
+		fmt.Println("DB open error")
+		return
+	}
+	statement, err := database.Prepare("CREATE TABLE IF NOT EXISTS logs (id INTEGER PRIMARY KEY, user TEXT, log TEXT)")
+	if err != nil {
+		fmt.Println(err)
+		fmt.Println("DB prepare error")
+		return
+	}
+	_, err = statement.Exec()
+	if err != nil {
+		fmt.Println(err)
+		fmt.Println("DB execute error")
+		return
+	}
 }
